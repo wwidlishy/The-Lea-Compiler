02d4110ce5b4b6a892540e2c9cdfaf37da72eca4
vodracseck <79907142+wwidlishy@users.noreply.github.com>
2024-01-07 00:01:57 +0100

    Started work on the lexer

--- source/lea.py
+++ source/lea.py
@@ -1,7 +1,15 @@
 """
-    IMPORTS
+    Imports
 """
 import os, sys
+import re
+
+"""
+    Global Constants
+"""
+
+DIGITS1 = "0123456789"
+DIGITS2 = ".xX0123456789AaBbCcDdEeFf"
 
 """
     Error
@@ -22,13 +30,27 @@ class Error:
 class Lexer:
     def __init__(self) -> None:
         self.tokens, self.index, self.pos, self.line = [], -1, 0, 1
+        self.mode, self.current = "", ""
 
     def lexicate(self, src) -> list:
         self.src = src
         while len(self.src) - self.index != 1:
             self._advance()
-            print(self._curchar())
+            if self.mode == "":
+                if self._curchar() in DIGITS1:
+                    self.mode = "number"
+                    self.current = self._curchar()
+                    continue
+            elif self.mode == "number":
+                if self._curchar() in DIGITS2:
+                    self.current += self._curchar()
+                else:
+                    self.make_number()
+                    sublexer = Lexer()
+                    self.tokens += sublexer.lexicate(self.src[self.index:])
+                    break
 
+        return self.tokens
     def _advance(self) -> None:
         self.index += 1
         self.pos   += 1
@@ -39,6 +61,21 @@ class Lexer:
     def _curchar(self) -> str:
         return self.src[self.index]
     
+    def make_number(self) -> None:
+        regex = [
+            [re.compile("[0-9][0-9]?"), "int(self.current.lstrip('0'))"], # Int Number Type
+            [re.compile("[0-9]?\.[0-9]?"), "float(self.current.lstrip('0'))"] # Float Number Type
+        ]
+        number = None
+        for rexpr, action in regex:
+            if len(re.findall(rexpr, self.current)) == 1:
+                 number = eval(action)
+
+        if number != None:
+            self.tokens.append([self.mode, number])
+        else:
+            error = Error(f"At {self.line}, {self.pos-len(self.current)}: Retarded Regex Error: Somehow you managed to find an issue with my regex.\nPlease open an issue at https://github.com/wwidlishy/The-Lea-Compiler/issues")
+            error.interupt()
 """
     Run
 """
@@ -56,4 +93,5 @@ else:
     error.interupt()
 
 lexer = Lexer()
-lexer.lexicate(file)
+tokens = lexer.lexicate(file)
+print(tokens)
